05ddd5e1409dbc0b1a1cc5cd1660b4f50e28ad67
Qiu <lopi258@outlook.com>
2022-01-08 18:50:38 +0800

    Moved projects to src folder.

---
+++ src/QTRHacker.Functions.Test/Program.cs
@@ -0,0 +1,37 @@
+﻿using QHackCLR.Common;
+using QHackCLR.DataTargets;
+using QHackLib;
+using QHackLib.Memory;
+using QTRHacker.Functions.GameObjects.Terraria;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace QTRHacker.Functions.Test
+{
+	class Program
+	{
+		unsafe static void Main()
+		{
+			using GameContext ctx = GameContext.OpenGame(Process.GetProcessesByName("Terraria")[0]);
+			ctx.Patches.Init();
+			//ctx.Patches.WorldPainter_EyeDropperActive = true;
+			/*using QHackContext ctx = QHackContext.Create(Process.GetProcessesByName("Terraria")[0].Id);
+			foreach (var m in ctx.CLRHelpers)
+			{
+				Console.WriteLine(m.Key.FileName);
+			}
+			var helper = ctx.CLRHelpers.Last(t => t.Key.Name == "QTRHacker.Patches").Key;
+			foreach (var t in helper.DefinedTypes)
+			{
+				Console.WriteLine(t.Name);
+			}*/
+			/*var helper = ctx.GetCLRHelper("Terraria");
+			var h = helper.GetStaticFieldAddress("Terraria.GameContent.TextureAssets", "MagicPixel");
+			Console.WriteLine(h.ToString("X8"));*/
+		}
+	}
+}
