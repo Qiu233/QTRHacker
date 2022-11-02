d0c91b90e5180126b3ed27b1c58c2664399cdc52
Amir <50082485+erxson@users.noreply.github.com>
2022-11-02 12:34:37 +0200

    Update UsefulPrefixes.cs

--- src/QTRHacker/Models/UsefulPrefixes.cs
+++ src/QTRHacker/Models/UsefulPrefixes.cs
@@ -2,17 +2,17 @@
 {
 	public enum UsefulPrefixes : byte
 	{
-		None=0,
-		Weapon=0,
-		Godly=59,
-		Legendary=81,
-		Unreal=82,
-		Demonic=60,
-		Mythical=83,
-		Mystic=26,
-		Def=65,
-		Mana=66,
-		Crit=68,
-		Damage=72
+		None = 0,
+		Weapon = 0,
+		Godly = 59,
+		Legendary = 81,
+		Unreal = 82,
+		Demonic = 60,
+		Mythical = 83,
+		Mystic = 26,
+		Def = 65,
+		Mana = 66,
+		Crit = 68,
+		Damage = 72
 	}
 }
